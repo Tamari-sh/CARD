c7673d3eca2f9b0fe8106f3dfead340221cfa2c1
Tamari-sh <tamari.sh2018@leyada.net>
2025-06-22 14:31:49 +0300

    listener context manager

--- Networking/listener.py
+++ Networking/listener.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+from connection import *
 from typing import Any
 
 
@@ -11,18 +12,38 @@ class Listener:
         self.host = host
         self.port = port
         self.backlog = backlog
+        self.listen_socket = socket.socket()
 
     def __repr__(self) -> str:
         """Create class representation"""
         return f"<Listener(port={self.port}, host={self.host}, backlog={self.backlog})>"
 
+    def __enter__(self) -> socket.socket:
+        """Implement context manager __enter__ method"""
+        return self.listen_socket
+
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        """Implement context manager __exit__ method"""
+        stop(self)
+
     def start(self) -> None:
         """Start Listener listening"""
 
-        listen_socket = socket.socket()
-        listen_socket.bind((self.host, self.port))
+        self.listen_socket.bind((self.host, self.port))
 
-        listen_socket.listen(self.backlog)
+        self.listen_socket.listen(self.backlog)
+
+    def stop(self) -> None:
+        """Close Listener Socket"""
+
+        self.listen_socket.close()
+
+    def accept(self) -> Connection:
+        """Waits for a connection, accepts it and return a connection"""
+
+        conn_socket, addr = self.listen_socket.accept()
+
+        return Connection(conn_socket)
 
 
 def main() -> None:
