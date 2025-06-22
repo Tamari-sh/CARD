311db09286fcb5bed4ca5536699ebca114f8ec0a
Tamari-sh <tamari.sh2018@leyada.net>
2025-06-22 14:03:31 +0300

    create listener __repr__

--- Networking/listener.py
+++ Networking/listener.py
@@ -4,8 +4,29 @@ from typing import Any
 
 class Listener:
     """Class for creating Listener"""
+
     def __init__(self, host: str, port: int, backlog: int = 1000) -> None:
         """Initialize the Listener class"""
         self.host = host
         self.port = port
         self.backlog = backlog
+
+    def __repr__(self) -> str:
+        """Create class representation"""
+        return f"<Listener(port={self.port}, host={self.host}, backlog={self.backlog})>"
+
+
+def main() -> None:
+    """
+    Implementation of Listener class.
+    """
+
+    with Listener(8000, '127.0.0.1') as listener:
+        with listener.accept() as connection:
+            print(connection)
+        # connection is closed
+    # listener is closed
+
+
+if __name__ == '__main__':
+    main()
