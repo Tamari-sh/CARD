a62408ad38b01ee75d7168636790abad51123bb7
Tamari-sh <tamari.sh2018@leyada.net>
2025-06-22 12:19:46 +0300

    implement __repr__ __init__

--- Networking/connection.py
+++ Networking/connection.py
@@ -2,4 +2,25 @@ from client import *
 from server import *
 
 
-class Connection():
+class Connection:
+    """Class for creating connection"""
+
+    def __init__(self, connection: socket.socket) -> None:
+        """Initialize the connection class"""
+        self.connection = connection
+
+    def __repr__(self) -> str:
+        """Create class representation"""
+        return f"<connection from {self.connection.getpeername()} to {self.connection.getsockname()}>"
+
+
+def main() -> None:
+    """
+    Implementation of Connection class.
+    """
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
